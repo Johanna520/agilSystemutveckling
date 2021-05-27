79698f5c68d61edfc3c41bc00f344298a23e48de
John <john.a.nilsson@hotmail.com>
2021-05-27 09:45:10 +0200

    Models tillagda, med en extra prop för Ingredients i SavedHamburgers

---
+++ IBBS/Models/Likes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBBS.Models
+{
+    public class Likes
+    {
+        public int Up { get; set; }
+        public int Down { get; set; }
+        public Users Users { get; set; }
+        public List<SavedHamburgers> Burgers { get; set; }
+
+    }
+}
