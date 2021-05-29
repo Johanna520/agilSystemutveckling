97b3ba2f0940e8ee62b4e252cde0c8ab86ff8369
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-05-29 13:11:31 +0200

    started user comment burger.

---
+++ IBBS/Pages/issueNr13.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IBBS.Data;
+using IBBS.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IBBS.Pages
+{
+    // this is a scaffolded creat-page!
+    public class issueNr13Model : PageModel
+    {
+        private readonly BurgerDbContext _context;
+        private readonly UserManager<Users> _userManager;
+        public issueNr13Model(BurgerDbContext context,
+            UserManager<Users> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Comments Comments { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Comments.Add(Comments);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
