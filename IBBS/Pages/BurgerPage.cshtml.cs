572e755662842d64df5962a1bf6edb8c1a417875
Sandra Lindberg <70195807+SandraLindberg1@users.noreply.github.com>
2021-06-02 14:50:21 +0200

    put authorize on burgerpage and create burger

--- IBBS/Pages/BurgerPage.cshtml.cs
+++ IBBS/Pages/BurgerPage.cshtml.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IBBS.Pages
 {
+
+    [Authorize]
     public class BurgerPageModel : PageModel
     {
         public void OnGet()
