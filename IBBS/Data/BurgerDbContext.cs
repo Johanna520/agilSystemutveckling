c876bd6e240cdee17e5fb3c2ba2c5b1e5679ff8d
John <john.a.nilsson@hotmail.com>
2021-06-03 14:15:32 +0200

    Lade till confirmation på remove account

--- IBBS/Data/BurgerDbContext.cs
+++ IBBS/Data/BurgerDbContext.cs
@@ -24,13 +24,16 @@ namespace IBBS.Data
         public async Task SeedAsync(UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
         {
 
-            //await this.Database.EnsureDeletedAsync(); //Use this to reset the database
+            await this.Database.EnsureDeletedAsync(); //Use this to reset the database
 
             bool isCreated = await this.Database.EnsureCreatedAsync();
             if (!isCreated)
             {
                 return;
             }
+
+            await roleManager.CreateAsync(new IdentityRole("Admin")); //Create roles
+            await roleManager.CreateAsync(new IdentityRole("User"));
 
             Users admin = new Users()  //Put in user info
             {
@@ -54,11 +57,28 @@ namespace IBBS.Data
             await userManager.CreateAsync(admin, "admin"); //Assign password
             await userManager.CreateAsync(user, "user1");
 
+            for (int i = 0; i < 10; i++)
+            {
+
+                Users users = new Users()
+                {
+                    FirstName = "User" + i,
+                    LastName = "Usersson" + i,
+                    UserName = "User" + i,
+                    Email = "User"+i+"@hotmail.com",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                };
+                await userManager.CreateAsync(users, "user"+i);
+
+                var userRoles = await Users.Where(u => u.Email == "User" + i + "@hotmail.com").FirstOrDefaultAsync();
+
+                await userManager.AddToRoleAsync(userRoles, "User");
+            }
+
             var adminRole = await Users.Where(u => u.Email == "Admin@hotmail.com").FirstOrDefaultAsync();
             var userRole = await Users.Where(u => u.Email == "User@hotmail.com").FirstOrDefaultAsync();
 
-            await roleManager.CreateAsync(new IdentityRole("Admin")); //Create roles
-            await roleManager.CreateAsync(new IdentityRole("User"));
 
             await userManager.AddToRoleAsync(adminRole, "Admin"); //Assign roles
             await userManager.AddToRoleAsync(userRole, "User");
---
+++ IBBS/wwwroot/js/UserList.js
@@ -0,0 +1,20 @@
+﻿//UserList Page
+
+let i = 0;
+let name = document.querySelectorAll(".testar");
+let names = Array.from(name);
+
+
+document.querySelectorAll(".removeFunction").forEach(item => {
+
+    item.addEventListener("submit", function (evt) {
+
+        let test = document.getElementById("test").value;
+        let r = confirm("Are you sure you want to delete this account? \n" + names[i].value);
+        if (r == false) {
+            evt.preventDefault();
+        }
+        i++;
+    })
+
+})
