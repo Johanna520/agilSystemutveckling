caaff4e6593e170b9b2dd3bf56fefa61e7272703
John <john.a.nilsson@hotmail.com>
2021-06-04 13:17:59 +0200

    klar

--- IBBS/wwwroot/js/UserList.js
+++ IBBS/wwwroot/js/UserList.js
@@ -1,18 +1,12 @@
 ﻿//UserList Page
 
-let i = 0;
-let name = document.querySelectorAll(".testar");
-let names = Array.from(name);
-
-
 document.querySelectorAll(".removeFunction").forEach(item => {
-    let hej = item;
     item.addEventListener("submit", function (e) {
-
-        let r = confirm("Are you sure you want to delete this account? \n" + names[i].value);
+        let r = confirm("Are you sure you want to delete this account?");
         if (r == false) {
             e.preventDefault();
         }
     })
 
 })
+
